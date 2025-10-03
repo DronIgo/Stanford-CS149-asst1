f59460bf8e8e2fb20f7ddf4fe6df053f8e7f9c38
Dron231 <a.igo@alumni.nsu.ru>
2025-10-03 21:29:36 +0700

    prog 1, gitignore

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <thread>
+#include <iostream>
 
 #include "CycleTimer.h"
 
@@ -8,6 +9,8 @@ typedef struct {
     float y0, y1;
     unsigned int width;
     unsigned int height;
+    int startRow;
+    int numRows;
     int maxIterations;
     int* output;
     int threadId;
@@ -28,15 +31,10 @@ extern void mandelbrotSerial(
 //
 // Thread entrypoint.
 void workerThreadStart(WorkerArgs * const args) {
-
-    // TODO FOR CS149 STUDENTS: Implement the body of the worker
-    // thread here. Each thread should make a call to mandelbrotSerial()
-    // to compute a part of the output image.  For example, in a
-    // program that uses two threads, thread 0 could compute the top
-    // half of the image and thread 1 could compute the bottom half.
-
-    printf("Hello world from thread %d\n", args->threadId);
+    mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width, args->height, args->startRow, args->numRows, args->maxIterations, args->output);
 }
+
+const int MAX_THREADS = 32;
 
 //
 // MandelbrotThread --
@@ -49,33 +47,32 @@ void mandelbrotThread(
     int width, int height,
     int maxIterations, int output[])
 {
-    static constexpr int MAX_THREADS = 32;
 
     if (numThreads > MAX_THREADS)
     {
         fprintf(stderr, "Error: Max allowed threads is %d\n", MAX_THREADS);
         exit(1);
     }
+    int rowPerThread = height / numThreads;
+    int extraRows = height % numThreads;
 
     // Creates thread objects that do not yet represent a thread.
     std::thread workers[MAX_THREADS];
     WorkerArgs args[MAX_THREADS];
-
+    int currentRow = 0;
     for (int i=0; i<numThreads; i++) {
-      
-        // TODO FOR CS149 STUDENTS: You may or may not wish to modify
-        // the per-thread arguments here.  The code below copies the
-        // same arguments for each thread
         args[i].x0 = x0;
         args[i].y0 = y0;
         args[i].x1 = x1;
         args[i].y1 = y1;
         args[i].width = width;
         args[i].height = height;
+        args[i].startRow = currentRow;
+        args[i].numRows = rowPerThread + (i == numThreads - 1 ? extraRows : 0);
         args[i].maxIterations = maxIterations;
         args[i].numThreads = numThreads;
         args[i].output = output;
-      
+        currentRow += rowPerThread;
         args[i].threadId = i;
     }
 
